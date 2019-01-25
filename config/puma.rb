67daf226a8478017cebf4eb74efd00b004c2495b
Sergey <sukyans@gmail.com>
2019-01-25 02:21:48 -0800

    update puma

--- config/puma.rb
+++ config/puma.rb
@@ -4,26 +4,10 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-# threads threads_count, threads_count
-#
-# port        ENV.fetch("PORT") { 3000 }
-#
-# environment ENV.fetch("RAILS_ENV") { "development" }
-# plugin :tmp_restart
-
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-preload_app!
+port        ENV.fetch("PORT") { 3000 }
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+environment ENV.fetch("RAILS_ENV") { "development" }
+plugin :tmp_restart
