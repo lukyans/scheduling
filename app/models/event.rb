9d112f1599286c238087e8fa544c037b3186bc6e
Sergey <sukyans@gmail.com>
2019-01-24 21:36:13 -0800

    add event dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :date, presence: true
+
+  has_one_attached :image
+  belongs_to :user, optional: true
+
+  scope :only_public,    -> { where(private: false) }
+  scope :only_private,   -> { where(private: true)  }
+  scope :by_recent,      -> { order(date: :asc) }
+
+  def start_time
+    self.date
+  end
 end
