a863fcdc4bba1e0321e7317390c1a429f0b49082
Sergey <sukyans@gmail.com>
2019-01-24 21:37:44 -0800

    add viewable by method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :events, dependent: :destroy
+
+  def self.viewable_by(viewing_user, user)
+    # return true unless (user.present?)
+
+    if viewing_user.present?
+      return true if viewing_user == user
+    end
+
+    return false
+  end
 end
