80d9c64a064004c6eac8f961cbc9fc1882ac67b6
Sergey <sukyans@gmail.com>
2019-01-25 18:29:20 -0800

    removed comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
 
   def self.viewable_by(viewing_user, user)
-    # return true unless (user.present?)
-
     if viewing_user.present?
       return true if viewing_user == user
     end
